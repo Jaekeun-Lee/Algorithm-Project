12719152793e9b0cfa466ef1ca941372d173d1fd
Jaekeun-Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-06-24 23:44:54 +0900

    Create Tourists.java

---
+++ test2_DFSBFS/Tourists.java
@@ -0,0 +1,25 @@
+package project.test2_DFSBFS;
+
+public class Tourists {
+	public String CityName;
+	public ToursInfo[] tourist;
+	
+	
+	
+	public Tourists(String name) {
+		this.CityName = name;
+		this.tourist = new ToursInfo[10];
+		for(int i = 0; i < 10; i++) {
+			
+		}
+		
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return CityName;
+	}
+	
+	
+}
