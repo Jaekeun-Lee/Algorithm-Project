1bbd32dbd1a9608385a357d1cde27fadacc4027d
Jaekeun-Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-06-24 23:44:58 +0900

    Create ToursInfo.java

---
+++ test2_DFSBFS/ToursInfo.java
@@ -0,0 +1,23 @@
+package project.test2_DFSBFS;
+
+public class ToursInfo {
+	public String name ="";
+	public String keyword ="" ;
+	public int count = 0;
+	
+	public ToursInfo(String name, String keyword,int count) {
+		this.name = name;
+		this.keyword = keyword;
+		this.count = count;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+	
+	
+
+	
+}
