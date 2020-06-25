7eed06d25d5941a8279be943f3ce6f94ca226755
Jaekeun-Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-06-25 17:08:17 +0900

    Create Edge.java

---
+++ test4_MinDist/Edge.java
@@ -0,0 +1,22 @@
+package project.test4_MinDist;
+
+public class Edge {
+
+	// 간선의 클래스 자료형
+	public int fv, tv; //출발 도시 도착도시
+	public int weight; // 거리
+	
+	
+
+	Edge() {
+		this.fv = 0;
+		this.tv = 0;
+		this.weight = 0;
+	}
+
+	Edge(int a, int b, int length) {
+		this.fv = a;
+		this.tv = b;
+		this.weight = length;
+	}
+}
